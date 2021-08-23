5586dc0a167b98eb8d7d3c7bd09a243b7098a6b7
Kana Otawara <shimmer22.0204@gmail.com>
2021-08-24 00:54:15 +0900

    add sample code

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -6,4 +6,75 @@ object Hello extends Greeting with App {
 
 trait Greeting {
   lazy val greeting: String = "hello"
+
+  val x = 3
+
+  /** case文を揃える */
+  x match {
+    case 3  => "ok 3"
+    case 44 => "ok 44"
+    case _  => "ng"
+  }
+
+  Map(
+    1  -> "a",
+    22 -> "b",
+  )
+
+  /** 1行あたりの文字数が多いと改行される */
+  new Hoge("aaa", 15)
+
+  /**
+    * TODO: メソッド定義が改行される
+    * testMethod(
+    *   hoge1, hoge2,
+    *   hoge3, hoge4, hoge5
+    * )
+    * のように中途半端なところで改行されていると、1行1つに直される
+    *
+    */
+  def testMethod(
+    hoge1: String,
+    hoge2: String,
+    hoge3: String,
+    hoge4: String,
+    hoge5: String
+  ) = {}
 }
+
+/**
+  * TODO:
+  * {
+  *   .....
+  * }
+  * が{}になってしまう
+  */
+case class Hoge(
+  name: String,
+  age:  Int
+) extends Fuga {}
+
+/**
+  * TODO:
+  * Seq [String]
+  * List[String]
+  * のように[を縦に揃えたい
+  */
+object Hoge {
+  type A = Seq[String]
+  type B = List[String]
+}
+
+abstract class Fuga {}
+
+/**
+  * TODO: scaladocのフォーマットがおかしい
+  */
+
+/**
+  * TODO: 空行が勝手に挿入されることがある
+  */
+
+/**
+  * TODO: extendsのインデント設定
+  */
